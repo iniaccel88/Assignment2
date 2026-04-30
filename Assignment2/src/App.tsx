9a97c3ce9b8250f75cc869dd4af3383bc15329e7
Joshua A. P. <ceritajoshua1289@gmail.com>
2026-04-30 15:38:36 +0800

    add pages

--- Assignment2/src/App.tsx
+++ Assignment2/src/App.tsx
@@ -1,12 +1,5 @@
+import Item from "./pages/Item"
 function App() {
-  return (
-    <div>
-      <h1>Menu</h1>
-      <p>Makanan</p>
-      <p>Minuman</p>
-      <p>Cemilan</p>
-    </div>
-  )
+	return <Item/>
 }
-
 export default App;
---
+++ Assignment2/src/pages/Item.tsx
@@ -0,0 +1,17 @@
+import {
+	ScatterChart,
+	Scatter,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+} from "recharts";
+
+function Item() {
+	return (
+		<div>
+			<h1>Regresi Linear - By Liong</h1>
+		</div>
+	)
+}
+export default Item;
